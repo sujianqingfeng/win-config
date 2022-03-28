034c19bb209d2e822e3d09172de068ad5e59e0f3
white-letter <121116111@qq.com>
2022-03-28 22:16:45 +0800

    feat: alias

--- terminal/Microsoft.PowerShell_profile.ps1
+++ terminal/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,17 @@
 # Import-Module oh-my-posh
 oh-my-posh --init --shell pwsh --config ~/OneDrive/terminal-theme/zash.omp.json | Invoke-Expression
 Import-Module posh-git
+
+
+# alias 
+function d {
+  nr dev
+}
+
+function ds{
+  nr serve
+}
+
+function b{
+  nr build
+}
