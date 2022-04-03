b70e6312160720600c99f28b925e1808a6dda6ab
white-letter <121116111@qq.com>
2022-04-03 22:26:40 +0800

    feat: add PSReadlione

--- terminal/Microsoft.PowerShell_profile.ps1
+++ terminal/Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,33 @@
 
+# Prompt
 # Import-Module oh-my-posh
-oh-my-posh --init --shell pwsh --config ~/OneDrive/terminal-theme/zash.omp.json | Invoke-Expression
 Import-Module posh-git
+Import-Module Terminal-Icons
+Install-Module PSReadLine
+
+oh-my-posh --init --shell pwsh --config ~/OneDrive/terminal-theme/zash.omp.json | Invoke-Expression
+
+
+#PSREadLine 
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -PredictionSource History
 
 # 删除冲突别名
 Del alias:ni -Force
 
-# alias 
+# Alias 
+
+Set-Alias vim nvim
+Set-Alias g git
+
+# foront
+# Set-Alias d nr dev 
+# Set-Alias ds nr server
+# Set-Alias db nr build
+# Set-Alias ns nr start
+
+
+
 function d {
   nr dev
 }
